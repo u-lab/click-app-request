43d5abadf6f42041e0c5673a01c53152ba3c5f73
H37kouya <h37kouya@gmail.com>
2021-08-16 20:47:02 +0900

    feature add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
